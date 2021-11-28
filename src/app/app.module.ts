2f615bef93fefe2bf87f10ebbb4961130dd5605d
Mohamed-Ibrahim-M <ibrahimnoorulla133@gmail.com>
2021-11-28 15:21:21 +0530

    app module change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MentionDirectiveDirective } from './mention-directive/mention-directive.directive';
 import { MentionItemsComponent } from './mention-items/mention-items.component';
-import { Mention2Directive } from './mention2.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     MentionDirectiveDirective,
     MentionItemsComponent,
-    Mention2Directive
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
